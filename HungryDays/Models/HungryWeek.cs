069eb623c76069815570422f04305abf95d3a218
Muhammet Demir <m.demir@ridder.com>
2022-05-29 01:44:59 +0200

    Deleted old mvc project

--- HungryDays/Models/HungryWeek.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace HungryDays.Models
-{
-    public class HungryWeek
-    {
-        public Guid Id { get; set; }
-        public List<HungryDay> AllDays { get; set; }
-        public List<Item> AllItems { get; set; }
-    }
-}
