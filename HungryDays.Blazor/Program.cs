3df4e3607d0f848d15c48e7cc9e90f1729b1b422
Muhammet Demir <m.demir@ridder.com>
2022-08-03 02:44:04 +0200

    Still fixing mssql db with ef (with docker)

--- HungryDays.Blazor/Program.cs
+++ HungryDays.Blazor/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using HungryDays.Database;
+using HungryDays.Database.Factories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,10 +11,33 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+
+//db
 builder.Services.AddDbContext<HungryDaysDbContext>();
+builder.Services.AddSingleton<HungryDaysDbContextFactory>();
 
 
 var app = builder.Build();
+CreateDbIfNotExists(app);
+
+//Configure db
+static void CreateDbIfNotExists(IHost host)
+{
+    using (var scope = host.Services.CreateScope())
+    {
+        var services = scope.ServiceProvider;
+        try
+        {
+            var context = services.GetRequiredService<HungryDaysDbContextFactory>();
+            DbInitializer.Initialize(context);
+        }
+        catch (Exception ex)
+        {
+            var logger = services.GetRequiredService<ILogger<Program>>();
+            logger.LogError(ex, "An error occurred creating the DB.");
+        }
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
