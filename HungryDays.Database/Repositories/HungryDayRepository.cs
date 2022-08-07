6b2990180de6221ca12f015fac9b9ee465aaf3e0
Muhammet Demir <m.demir@ridder.com>
2022-08-08 00:48:11 +0200

    making repositories for interaction with db

---
+++ HungryDays.Database/Repositories/HungryDayRepository.cs
@@ -0,0 +1,39 @@
+﻿using HungryDays.Database.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HungryDays.Database.Repositories
+{
+    public class HungryDayRepository
+    {
+        private HungryDaysDbContext _dbContext;
+        public HungryDayRepository(HungryDaysDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<IEnumerable<HungryDayEntity>> GetHungryDaysAsync()
+        {
+            return await _dbContext.HungryDays
+                .OrderBy(x => x.Id)
+                .ToListAsync();
+        }
+
+        public async Task<HungryDayEntity> GetHungryDayAsync(int id)
+        {
+            return await _dbContext.HungryDays
+                .FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task UpdateHungryDayAsync(HungryDayEntity hungryDayEntity)
+        {
+            _dbContext.HungryDays.Update(hungryDayEntity);
+
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
---
+++ HungryDays.Database/Repositories/HungryItemRepository.cs
@@ -0,0 +1,32 @@
+﻿using HungryDays.Database.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HungryDays.Database.Repositories
+{
+    public class HungryItemRepository
+    {
+        private HungryDaysDbContext _dbContext;
+        public HungryItemRepository(HungryDaysDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<IEnumerable<HungryItemEntity>> GetHungryItemsAsync()
+        {
+            return await _dbContext.HungryItems
+                .OrderBy(x => x.Id)
+                .ToListAsync();
+        }
+
+        public async Task<HungryItemEntity> GetHungryItemAsync(Guid id)
+        {
+            return await _dbContext.HungryItems
+                .FirstOrDefaultAsync(x => x.Id == id);
+        }
+    }
+}
